3413c646cbd2cb0791bc1a833bbd6c7b36d1e6e1
Michal Gruszka <m.gruszka215@gmail.com>
2018-06-03 08:26:46 +0200

    Config window size

--- core/src/com/mygdx/game/MinesweeperGame.java
+++ core/src/com/mygdx/game/MinesweeperGame.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.Game;
 
 public class MinesweeperGame extends Game {
 
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+
     @Override
     public void create() {
         setScreen(new MainMenuScreen());
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.mygdx.game.MinesweeperGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = MinesweeperGame.WIDTH;
+		config.height = MinesweeperGame.HEIGHT;
 		new LwjglApplication(new MinesweeperGame(), config);
 	}
 }
