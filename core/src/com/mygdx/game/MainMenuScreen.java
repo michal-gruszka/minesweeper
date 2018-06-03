ac9b5fb3bc7abe64e8493bc9554e6fc3f28332d9
Michal Gruszka <m.gruszka215@gmail.com>
2018-06-03 08:45:53 +0200

    Render placeholder background

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -1,7 +1,33 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class MainMenuScreen extends ScreenAdapter {
-    
+
+    MinesweeperGame game;
+
+    public MainMenuScreen(MinesweeperGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.getShapeRenderer().begin(ShapeRenderer.ShapeType.Filled);
+        game.getShapeRenderer().rect(
+                0,
+                0,
+                MinesweeperGame.WIDTH,
+                MinesweeperGame.HEIGHT,
+                Color.DARK_GRAY,
+                Color.DARK_GRAY,
+                Color.LIGHT_GRAY,
+                Color.LIGHT_GRAY
+                );
+        game.getShapeRenderer().end();
+    }
 }
--- core/src/com/mygdx/game/MinesweeperGame.java
+++ core/src/com/mygdx/game/MinesweeperGame.java
@@ -1,14 +1,29 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class MinesweeperGame extends Game {
 
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
 
+    private ShapeRenderer shapeRenderer;
+    private SpriteBatch spriteBatch;
+
     @Override
     public void create() {
-        setScreen(new MainMenuScreen());
+        setScreen(new MainMenuScreen(this));
+        this.shapeRenderer = new ShapeRenderer();
+        this.spriteBatch = new SpriteBatch();
+    }
+
+    public ShapeRenderer getShapeRenderer() {
+        return shapeRenderer;
+    }
+
+    public SpriteBatch getSpriteBatch() {
+        return spriteBatch;
     }
 }
