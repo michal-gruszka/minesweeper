a4f0bf1d3407ac78c7c0a8400d31437857ed5cb5
Michal Gruszka <m.gruszka215@gmail.com>
2018-06-03 23:04:58 +0200

    Add play, highscores, about buttons

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -17,8 +17,22 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MainMenuScreen extends ScreenAdapter {
 
+    private static final String PLAY_BUTTON = "play_button_silver";
+    private static final String PLAY_BUTTON_HOVER = "play_button_gold";
+    private static final String HIGHSCORES_BUTTON = "highscores_button_silver";
+    private static final String HIGHSCORES_BUTTON_HOVER = "highscores_button_gold";
+    private static final String ABOUT_BUTTON = "about_button_silver";
+    private static final String ABOUT_BUTTON_HOVER = "about_button_gold";
     private static final String EXIT_BUTTON = "exit_button_silver";
     private static final String EXIT_BUTTON_HOVER = "exit_button_gold";
+    private static final int FIRST_MENU_ITEM_YPOS = (int) (MinesweeperGame.HEIGHT * 0.5);
+    private static final int BUTTON_HEIGHT = 40;
+    private static final int BUTTON_SPACING = 20;
+
+    private static final int PLAY_BUTTON_YPOS = FIRST_MENU_ITEM_YPOS;
+    private static final int HIGHSCORES_BUTTON_YPOS = FIRST_MENU_ITEM_YPOS - (BUTTON_HEIGHT + BUTTON_SPACING) * 1;
+    private static final int ABOUT_BUTTON_YPOS = FIRST_MENU_ITEM_YPOS - (BUTTON_HEIGHT + BUTTON_SPACING) * 2;
+    private static final int EXIT_BUTTON_YPOS = FIRST_MENU_ITEM_YPOS - (BUTTON_HEIGHT + BUTTON_SPACING) * 3;
 
     private MinesweeperGame game;
     private Stage stage;
@@ -26,12 +40,16 @@ public class MainMenuScreen extends ScreenAdapter {
     private Skin buttonSkin;
 
     public MainMenuScreen(MinesweeperGame game) {
+        System.out.println(PLAY_BUTTON_YPOS);
         this.game = game;
 
         buttonAtlas = new TextureAtlas(Gdx.files.internal("pack.atlas"));
         buttonSkin = new Skin(buttonAtlas);
 
         stage = new Stage(new ScreenViewport());
+        stage.addActor(createPlayButton(buttonSkin));
+        stage.addActor(createHighscoresButton(buttonSkin));
+        stage.addActor(createAboutButton(buttonSkin));
         stage.addActor(createExitButton(buttonSkin));
         Gdx.input.setInputProcessor(stage);
     }
@@ -59,6 +77,54 @@ public class MainMenuScreen extends ScreenAdapter {
         renderer.end();
     }
 
+    private ImageButton createPlayButton(Skin skin) {
+
+        ImageButtonStyle buttonStyle = new ImageButtonStyle();
+        buttonStyle.up = skin.getDrawable(PLAY_BUTTON);
+        buttonStyle.over = skin.getDrawable(PLAY_BUTTON_HOVER);
+
+        ImageButton exitButton = new ImageButton(buttonStyle);
+
+        exitButton.setPosition(
+                (MinesweeperGame.WIDTH - exitButton.getWidth()) / 2,
+                PLAY_BUTTON_YPOS
+        );
+        exitButton.addListener(playButtonListener());
+        return exitButton;
+    }
+
+    private ImageButton createHighscoresButton(Skin skin) {
+
+        ImageButtonStyle buttonStyle = new ImageButtonStyle();
+        buttonStyle.up = skin.getDrawable(HIGHSCORES_BUTTON);
+        buttonStyle.over = skin.getDrawable(HIGHSCORES_BUTTON_HOVER);
+
+        ImageButton exitButton = new ImageButton(buttonStyle);
+
+        exitButton.setPosition(
+                (MinesweeperGame.WIDTH - exitButton.getWidth()) / 2,
+                HIGHSCORES_BUTTON_YPOS
+        );
+        exitButton.addListener(highscoresButtonListener());
+        return exitButton;
+    }
+
+    private ImageButton createAboutButton(Skin skin) {
+
+        ImageButtonStyle buttonStyle = new ImageButtonStyle();
+        buttonStyle.up = skin.getDrawable(ABOUT_BUTTON);
+        buttonStyle.over = skin.getDrawable(ABOUT_BUTTON_HOVER);
+
+        ImageButton exitButton = new ImageButton(buttonStyle);
+
+        exitButton.setPosition(
+                (MinesweeperGame.WIDTH - exitButton.getWidth()) / 2,
+                ABOUT_BUTTON_YPOS
+        );
+        exitButton.addListener(aboutButtonListener());
+        return exitButton;
+    }
+
     private ImageButton createExitButton(Skin skin) {
 
         ImageButtonStyle buttonStyle = new ImageButtonStyle();
@@ -69,10 +135,22 @@ public class MainMenuScreen extends ScreenAdapter {
 
         exitButton.setPosition(
                 (MinesweeperGame.WIDTH - exitButton.getWidth()) / 2,
-                (MinesweeperGame.HEIGHT - exitButton.getHeight()) / 2
+                EXIT_BUTTON_YPOS
         );
         exitButton.addListener(exitButtonListener());
         return exitButton;
+    }
+
+    private ClickListener playButtonListener() {
+        return new ClickListener();
+    }
+
+    private ClickListener highscoresButtonListener() {
+        return new ClickListener();
+    }
+
+    private ClickListener aboutButtonListener() {
+        return new ClickListener();
     }
 
     private ClickListener exitButtonListener() {
